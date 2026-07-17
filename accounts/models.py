828c293b2e60a0751ec85d57d683cf0dbbb7b22a
mahvash <mohammadmahvash1@gmail.com>
2026-07-17 11:06:41 +0330

    user manager

---
+++ accounts/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from .managers import UserManager
+
+
+class User(AbstractBaseUser):
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
+    phone_number = models.CharField(max_length=11, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = [ 'email' , 'full_name' ]
+
+    def __str__(self):
+        return f'email: {self.email} \br full name: {self.full_name}'
