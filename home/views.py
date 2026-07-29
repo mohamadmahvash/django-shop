393bc0e6f8942efa591ce2a9f1e767724f1843f7
mahvash <mohammadmahvash1@gmail.com>
2026-07-29 19:35:40 +0330

    user pass test

--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from django.contrib import messages
 
+from utils import IsAdminUserMixin
 from products.models import Product
 from . import tasks
 
@@ -12,7 +13,7 @@ class HomeView(View):
         return render(request, 'home/index.html', {'products': products})
 
 
-class BucketHomeView(View):
+class BucketHomeView(IsAdminUserMixin,View):
     template_name = 'home/bucket.html'
 
     def get(self, request):
@@ -20,14 +21,14 @@ class BucketHomeView(View):
         return render(request, self.template_name, {'objects': objects})
 
 
-class DeleteBucketObjectView(View):
+class DeleteBucketObjectView(IsAdminUserMixin,View):
     def get(self, request, key):
         tasks.delete_object_task.delay(key)
         messages.success(request, 'Object deleted successfully', extra_tags='info')
         return redirect('home:bucket')
 
 
-class DownloadBucketObjectView(View):
+class DownloadBucketObjectView(IsAdminUserMixin,View):
     def get(self, request, key):
         tasks.download_object_task.delay(key)
         messages.success(request, 'Object downloaded successfully', extra_tags='info')
--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
-from kavenegar import *
-
+from kavenegar import KavenegarAPI
+from django.contrib.auth.mixins import UserPassesTestMixin
 
 def send_otp_code(phone_number, code):
 
@@ -11,3 +11,7 @@ def send_otp_code(phone_number, code):
 
     except Exception as e:
         print(e)
+
+class IsAdminUserMixin(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.is_authenticated and self.request.user.is_admin
