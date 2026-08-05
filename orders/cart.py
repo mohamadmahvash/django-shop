cfa44e31c71852d3fafaf420213904d7e17b6324
mahvash <mohammadmahvash1@gmail.com>
2026-08-05 10:12:19 +0330

    checkout

--- orders/cart.py
+++ orders/cart.py
@@ -40,5 +40,9 @@ class Cart:
     def save(self):
         self.session.modified = True
 
+    def clear(self):
+        del self.session[CART_SESSION_ID]
+        self.save()
+
     def get_cart_total_price(self):
         return sum(item["total_price"] for item in self.cart.values())
--- orders/urls.py
+++ orders/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('cart/', views.CartView.as_view(), name='cart'),
     path('cart/add/<int:product_id>/', views.CartAddView.as_view(), name='cart_add'),
     path('cart/delete/<int:product_id>/', views.CartDeleteView.as_view(), name='cart_delete'),
+    path('create/', views.OrderCreateView.as_view(), name='order_create'),
+    path('detail/<int:order_id>/', views.OrderDetailView.as_view(), name='order_detail'),
 ]
