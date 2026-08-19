06f96af8dc675e2fd55642b313c1a281845d6db2
mahvash <mohammadmahvash1@gmail.com>
2026-08-19 21:35:07 +0330

    redis cache

--- shop/settings.py
+++ shop/settings.py
@@ -98,6 +98,15 @@ DATABASES = {
     }
 }
 
+# CACHE
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django.core.cache.backends.redis.RedisCache",
+#         "LOCATION": "redis://127.0.0.1:6379",
+#     }
+# }
+# SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
